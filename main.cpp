0194808d440ec89226c0122b7d8f27342c75eeae
takashithiba <k021g1120@g.neec.ac.jp>
2023-10-02 15:35:08 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+#include<cstdlib>
+
+int main(int argc,char* argv[])
+{
+	//argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (int i=0;i<argc;i++)
+	{
+		//•¶š—ñargv‚Ìi”Ô‚ğ•\¦
+		printf(argv[i]);
+		//‰üs
+		printf("\n");
+	}
+
+	system("pause");
+	return 0;
+}
